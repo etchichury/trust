0ec1f7effeb65d580530738dee38ca48fbc0283b
etchichury <joaoetchichury@hotmail.com>
2022-04-28 01:00:38 -0300

    Parse IPv4 packet

--- src/main.rs
+++ src/main.rs
@@ -5,15 +5,22 @@ fn main() -> io::Result<()> {
     let mut buf = [0u8; 1504];
     loop {
         let nbytes = nic.recv(&mut buf[..])?;
-        let flags = u16::from_be_bytes([buf[0], buf[1]]);
-        let proto = u16::from_be_bytes([buf[2], buf[3]]);
-        eprintln!(
-            "read {} bytes: (flags: {:x}, proto: {:x}) {:x?}",
-            nbytes - 4,
-            flags,
-            proto,
-            &buf[4..nbytes]
-        );
+        let _eth_flags = u16::from_be_bytes([buf[0], buf[1]]);
+        let eth_proto = u16::from_be_bytes([buf[2], buf[3]]); //ethernet frame protocol
+        if eth_proto != 0x0800 {
+            // not IPv4
+            continue;
+        }
+        match etherparse::Ipv4HeaderSlice::from_slice(&buf[4..nbytes]) {
+            Ok(packet) => {
+                let src = packet.source_addr();
+                let destination = packet.destination_addr();
+                let protocol = packet.protocol(); // IP level protocol (expect: TCP)
+                eprintln!("{} →  {} | {} bytes of protocol {}", src, destination, packet.payload_len(), protocol);
+            },
+            Err(e) => {
+                eprintln!("Ignoring non IPv4 packet {:?}", e)
+            }
+        }
     }
-    Ok(())
 }
